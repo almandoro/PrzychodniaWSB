fb1e9bd9fa84959f826bb5d0c19529da6177065c
almandoro <49877673+almandoro@users.noreply.github.com>
2020-05-31 17:50:56 +0200

    Add Entities

---
+++ Core/Entity/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzychodniaWSB.Core.Clinic.Entity {
+    public class Patient : AbstractPerson {
+
+        public  Patient(string name, string lastName) : base(name, lastName) {
+        }
+    }
+}
